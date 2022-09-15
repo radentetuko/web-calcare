45a84e92681c0fc8ca4f3a38d60d196126122aea
radentetuko <90023021+radentetuko@users.noreply.github.com>
2022-09-15 14:15:29 +0700

    Add files via upload

---
+++ training/login.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="Login.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@800&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+
+<body>
+    <nav class="navbar sticky-top navbar-expand-lg navbar-light ">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="img/CalcareLogo.png" alt="">
+            </a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse text-right" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#">Makanan</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Tracking Kalori</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Tracking Diet</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Dampak Karbon</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><i class="bi bi-bell"></i></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><i class="bi bi-person-circle"></i></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <main>
+        <div class="container">
+            <div class="kiri">
+                <img src="img/logoLogin.png" alt="logo login">
+            </div>
+            <div class="kanan">
+                <img src="img/CalcareLogo.png" alt="logo calcare">
+                <h1>Calcare</h1>
+                <h2>Masuk</h2>
+                <h5>Selamat Datang Di Calcare</h5>
+            </div>
+        </div>
+    </main>
+
+    <footer>
+        <div class="container-fluid">
+            <div class="kiri">
+                <img src="img/CalcareLogo.png" alt="">
+                <h5>Keep your calories for your health</h5>
+                <p>Jl. H. R. Rasuna Said Daerah Khusus Ibukota Jakarta 12950</p>
+                <div class="medsos">
+                    <h5 style="color: white;">
+                        Ikuti Kami
+                    </h5>
+                    <a href="#" style="color: white;"><i class="bi bi-facebook"></i></a>
+                    <a href="#" style="color: white;"><i class="bi bi-instagram"></i></a>
+                    <a href="#" style="color: white;"><i class="bi bi-twitter"></i></a>
+                    <a href="#" style="color: white;"><i class="bi bi-youtube"></i></a>
+                </div>
+            </div>
+
+            <div class="tengah">
+                <h5>Layanan</h5>
+                <p>Tentang Kami</p>
+                <p>Kebijakan dan Privasi</p>
+                <p>Hubungi Kami</p>
+                <p>bantuan</p>
+            </div>
+
+            <div class="kanan">
+                <h5>Challenge Partner</h5>
+                <img src="img/logoDanone.png" alt="">
+                <h5>Mitra</h5>
+                <img src="img/logoSkilvul.svg" style="width: 100px;" alt="">
+            </div>
+        </div>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
+    </script>
+
+</body>
+
+</html>
